c75958ed8294c9b8791c60c33ba969678351960b
lburgazzoli <lburgazzoli@gmail.com>
2017-11-22 15:56:53 +0100

    init

---
+++ src/main/java/io/syndesis/extension/SimpleExtension.java
@@ -0,0 +1,42 @@
+package io.syndesis.extension;
+
+import java.io.IOException;
+
+import org.apache.camel.Body;
+import org.apache.camel.util.ObjectHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+import io.syndesis.integration.runtime.api.SyndesisExtensionAction;
+
+public class SimpleExtension {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleExtension.class);
+    private boolean ascii;
+
+    public void setAscii(boolean ascii) {
+        this.ascii = ascii;
+    }
+
+    public boolean isAscii() {
+        return this.ascii;
+    }
+
+    @SyndesisExtensionAction(
+        id = "log-body", 
+        name = "simple-log", 
+        description = "A simple function based logging extension (1)"
+    )
+    public void log(@Body String body) {
+        try {
+            if (ascii) {
+                LOGGER.info("Body is: \n{}", FigletFont.convertOneLine(body));
+            } else {
+                LOGGER.info("Body is: {}", body);
+            }
+        } catch (IOException e) {
+            throw ObjectHelper.wrapRuntimeCamelException(e);
+        }
+    }
+}
